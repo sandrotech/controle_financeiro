e9119dd7b6556c05be3d6782ca19d46613b15abc
Alessandro Barbosa <sandro.santostech@gmail.com>
2023-08-26 18:23:00 -0300

    adicionar template

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'controle_financeiro',
-    'controle_visual'
+    'controle_visual',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from controle_financeiro.views import RegistroViewSet
-
+from controle_visual import views
 router = DefaultRouter()
 router.register(r'registros', RegistroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('gerenciamento/', include(controle_visual.urls)),
+    path('telaInicial/', views.home , name="home"),
 ]
